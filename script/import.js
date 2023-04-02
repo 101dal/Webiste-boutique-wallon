1121d1bb27474dd8fd88de38a9cd2139a81374ed
101dal <66965262+101dal@users.noreply.github.com>
2023-04-02 20:41:44 +0200

    Modification d'import et de html + refonte graphique + modifiction fonctionnement page boutique

--- script/import.js
+++ script/import.js
@@ -1,7 +1,11 @@
 function redirect(e,f) {
 	content = $('.content');
 	element = $(f)
-	content.load(e);
+	content.css("opacity", "0");
+	//setTimeout(() => {
+		content.load(e);
+		content.css("opacity", "1");
+	//}, 200);
 	$('.active').removeClass('active');
 	element.parent().addClass('active');
 }
