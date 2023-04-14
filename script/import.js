962a767ab953832a796ce7937e697272e821bd62
101dal <4289927-101dal@users.noreply.replit.com>
2023-04-14 07:22:26 +0000

    Modified the style of the buttons

--- script/import.js
+++ script/import.js
@@ -1,11 +1,11 @@
-function redirect(e,f) {
+function redirect(e, f) {
 	content = $('.content');
 	element = $(f)
 	content.css("opacity", "0");
 	//setTimeout(() => {
-		content.load(e);
-		content.css("opacity", "1");
+	content.load(e);
+	content.css("opacity", "1");
 	//}, 200);
 	$('.active').removeClass('active');
-	element.parent().addClass('active');
+	element.addClass('active');
 }
